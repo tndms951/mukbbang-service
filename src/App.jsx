3dda3aa4ec64827ec9ab014b629c5589318fe992
송지수 <tndms951@naver.com>
2021-02-03 20:41:02 +0900

    첫번째 커밋

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Signin from './components/sign-in/signin_component';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Signin />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/sign-in/signin_component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './signin_style.css';
+import '../../utils/reset.css';
+
+function Signin() {
+  console.log('asd');
+  return (
+    <>
+      <div className="aaa">
+        <div className="signin_all_wrap">
+          <div className="signin">
+            <h3>로그인</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Signin;
