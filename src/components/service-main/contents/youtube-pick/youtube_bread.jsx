74dc70b30d15aab5883723b5e83dc2e1b0b4f6af
송지수 <tndms951@naver.com>
2021-03-20 12:54:46 +0900

    youtube-pickBread component add react-slick

--- src/components/service-main/contents/youtube-pick/youtube_bread.jsx
+++ src/components/service-main/contents/youtube-pick/youtube_bread.jsx
@@ -1,12 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect } from 'react';
 import Youtube from 'react-youtube';
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Swiper } from 'swiper/react';
 // import 'swiper/css/swiper.scss';
 
+import Slider from 'react-slick';
 import axios from '../../../../utils/axios';
 import { errorhandler } from '../../../../utils/common';
 
-import { BreadPickWrap, YoutubePickEvent, PickBreadTitle, PickBreadImage, Aaa } from './youtube_bread_style';
+// eslint-disable-next-line object-curly-newline
+import {
+  BreadPickWrap,
+  YoutubePickEvent,
+  StyledSlider,
+  PickBreadTitle,
+  PickBreadImage
+  // eslint-disable-next-line object-curly-newline
+} from './youtube_bread_style';
 
 const PickBread = () => {
   useEffect(() => {
@@ -33,6 +43,38 @@ const PickBread = () => {
     e.target.pauseVideo();
   };
 
+  // 슬라이드
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+
+    responsive: [
+      // 반응형 웹 구현 옵션
+      {
+        breakpoint: 1200, // 화면 사이즈 1200px
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  };
+
   return (
     <BreadPickWrap>
       <YoutubePickEvent />
@@ -41,46 +83,70 @@ const PickBread = () => {
         <span>유튜버들이 선택한 빵을 즐겨보세요</span>
       </PickBreadTitle>
 
-      <Swiper
+      {/* <Swiper
         navigation
         pagination={{
           clickable: true
         }}
         scrollbar={{
           draggable: true
-        }}
-      >
-
-        <Aaa>
-          <SwiperSlide>
-            <PickBreadImage>
-              <Youtube className="youtubeVideo" videoId="2g811Eo7K8U" opts={opts} onReady={onReady} />
-              <div className="pick_title">
-                <span>Title</span>
-                <span>Description</span>
-                <button type="button" className="show_button">리뷰 보러가기</button>
-              </div>
-            </PickBreadImage>
-            <PickBreadImage>
-              <Youtube className="youtubeVideo" videoId="2g811Eo7K8U" opts={opts} onReady={onReady} />
-              <div className="pick_title">
-                <span>Title</span>
-                <span>Description</span>
-                <button type="button" className="show_button">리뷰 보러가기</button>
-              </div>
-            </PickBreadImage>
-            <PickBreadImage>
-              <Youtube className="youtubeVideo" videoId="2g811Eo7K8U" opts={opts} onReady={onReady} />
-              <div className="pick_title">
-                <span>Title</span>
-                <span>Description</span>
-                <button type="button" className="show_button">리뷰 보러가기</button>
-              </div>
-            </PickBreadImage>
-          </SwiperSlide>
-        </Aaa>
-
-      </Swiper>
+        }}> */}
+      <Slider {...settings}>
+        <StyledSlider>
+          <PickBreadImage>
+            <div className="VideoContainer">
+              <Youtube
+                className="youtubeVideo"
+                videoId="2g811Eo7K8U"
+                opts={opts}
+                onReady={onReady}
+              />
+            </div>
+            <div className="pick_title">
+              <span>Title</span>
+              <span>Description</span>
+              <button type="button" className="show_button">
+                리뷰 보러가기
+              </button>
+            </div>
+          </PickBreadImage>
+          <PickBreadImage>
+            <div className="VideoContainer">
+              <Youtube
+                className="youtubeVideo"
+                videoId="2g811Eo7K8U"
+                opts={opts}
+                onReady={onReady}
+              />
+            </div>
+            <div className="pick_title">
+              <span>Title</span>
+              <span>Description</span>
+              <button type="button" className="show_button">
+                리뷰 보러가기
+              </button>
+            </div>
+          </PickBreadImage>
+          <PickBreadImage>
+            <div className="VideoContainer">
+              <Youtube
+                className="youtubeVideo VideoContainer"
+                videoId="2g811Eo7K8U"
+                opts={opts}
+                onReady={onReady}
+              />
+            </div>
+            <div className="pick_title">
+              <span>Title</span>
+              <span>Description</span>
+              <button type="button" className="show_button">
+                리뷰 보러가기
+              </button>
+            </div>
+          </PickBreadImage>
+        </StyledSlider>
+      </Slider>
+      {/* </Swiper> */}
     </BreadPickWrap>
   );
 };
