8c7ebf893bd4b617ff4218b90dec97850a8bf72d
송지수 <tndms951@naver.com>
2021-04-07 17:02:11 +0900

    detaile-breadShop design finsh

--- src/components/service-main/contents/breadhouse-rangking/detaile/breadhouse_detaile.jsx
+++ src/components/service-main/contents/breadhouse-rangking/detaile/breadhouse_detaile.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
-import { HouseDetaile, Grade, ShopImage, Information } from './breadhouse_detaile_style';
+import {
+  HouseDetaile,
+  Grade,
+  ShopImage,
+  Information,
+  Review,
+  ReviewBox,
+  BoxButton,
+  BoxLeft,
+  BoxRight,
+  Content,
+  UserImage,
+  OtherBreadShop,
+  BreadShopList
+} from './breadhouse_detaile_style';
 
 const ShopDetaile = () => {
   console.log('디테일');
@@ -22,7 +36,86 @@ const ShopDetaile = () => {
             <div>주소</div>
             <p>Description</p>
           </Information>
+          <Information>
+            <div>전화번호</div>
+            <p>010-0000-0000</p>
+          </Information>
+          <Information>
+            <div>음식종류</div>
+            <p>Description</p>
+          </Information>
+          <Information>
+            <div>가격</div>
+            <p>Description</p>
+          </Information>
+          <Information>
+            <div>주차</div>
+            <p>Description</p>
+          </Information>
+          <Information>
+            <div>영업시간</div>
+            <p>Description</p>
+          </Information>
+          <Information>
+            <div>홈페이지</div>
+            <p>www.naver.com</p>
+          </Information>
         </ShopImage>
+
+        <Review>
+          <h1>리뷰</h1>
+        </Review>
+
+        <ReviewBox>
+          <BoxButton>
+            <BoxLeft>
+              <img src="" alt="" />
+              <p>User</p>
+              <p>2020.12.29</p>
+            </BoxLeft>
+
+            <BoxRight>
+              <button type="submit">좋아요</button>
+              <button type="submit">별로예요</button>
+            </BoxRight>
+          </BoxButton>
+
+          <Content>
+            <p>어쩌구저쩌구~~어쩌구저쩌구~~어쩌구저쩌구~~어쩌구저쩌구~~123445566778900</p>
+            <p>어쩌구저쩌구~~어쩌구저쩌구~~어쩌구저쩌구~~어쩌구저쩌구~~123445566778900</p>
+            <p>어쩌구저쩌구~~어쩌구저쩌구~~어쩌구저쩌구~~어쩌구저쩌구~~123445566778900</p>
+          </Content>
+
+          <UserImage>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </UserImage>
+        </ReviewBox>
+
+        <OtherBreadShop>
+          <h1>빵집 랭킹</h1>
+          <div className="all_show">
+            <span>모두보기</span>
+            <span className="triangle" />
+          </div>
+        </OtherBreadShop>
+
+        <BreadShopList>
+          <ul className="list_wrap">
+            {/* {breadShopList.map((breadShopData) => ( */}
+            <li>
+              <img src="" alt="" />
+              <dl>
+                <dt>ss</dt>
+                <dd>ss</dd>
+              </dl>
+            </li>
+            {/* ))} */}
+          </ul>
+        </BreadShopList>
       </HouseDetaile>
     </>
   );
--- src/components/service-main/contents/breadhouse-rangking/detaile/breadhouse_detaile_style.jsx
+++ src/components/service-main/contents/breadhouse-rangking/detaile/breadhouse_detaile_style.jsx
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 export const HouseDetaile = styled.div`
   margin: 0 auto;
   margin-top: 110px;
+  width: 1024px;
 `;
 
 export const Grade = styled.div`
-  outline: 1px solid red;
   padding: 8px 24px;
 
   &::after {
@@ -21,15 +21,13 @@ export const Grade = styled.div`
     float: left;
     margin-left: 168px;
     width: 225px;
-    outline: 1px solid blue;
   }
 
   & span {
-    color: orange;
+    color: #fb7819;
   }
 
   & img {
-    outline: 1px solid red;
     line-height: 100px;
     float: right;
     width: 30px;
@@ -50,8 +48,7 @@ export const ShopImage = styled.div`
 `;
 
 export const Information = styled.div`
-  outline: 3px solid red;
-
+  margin-bottom: 16px;
   &::after {
     clear: both;
     content: '';
@@ -61,7 +58,6 @@ export const Information = styled.div`
   div {
     font-size: 16px;
     font-weight: 700;
-    outline: 2px solid blue;
     width: 213px;
     float: left;
   }
@@ -71,4 +67,177 @@ export const Information = styled.div`
     float: left;
     width: 380px;
   }
+`;
+
+export const Review = styled.div`
+  
+  margin: 64px 0 29px; 0;
+  padding: 8px 192px;
+  font-size: 32px;
+  font-weight: 700;
+`;
+
+export const ReviewBox = styled.div`
+  padding: 32px 192px;
+  background: #f8f8f8;
+`;
+
+export const BoxButton = styled.div`
+  outline: 2px solid blue;
+
+  &::after {
+    clear: both;
+    content: '';
+    display: block;
+  }
+`;
+
+export const BoxLeft = styled.div`
+  & img {
+    background: #c4c4c4;
+    border-radius: 50%;
+    width: 48px;
+    height: 48px;
+    display: block;
+    float: left;
+    margin-right: 14px;
+  }
+
+  & p:nth-child(2) {
+    font-size: 16px;
+    font-weight: 700;
+    margin-bottom: 6px;
+  }
+
+  & p:nth-child(3) {
+    color: #9a9a9a;
+  }
+`;
+
+export const BoxRight = styled.div`
+  outline: 1px solid red;
+  float: right;
+
+  div {
+    vertical-align: middle;
+  }
+`;
+
+export const Content = styled.div`
+  margin-top: 23px;
+`;
+
+export const UserImage = styled.div`
+  outline: 1px solid red;
+  display: block;
+  margin-top: 24px;
+
+  & img {
+    width: 96px;
+    height: 96px;
+    margin-right: 24px;
+    background: #c4c4c4;
+  }
+`;
+
+export const OtherBreadShop = styled.div`
+padding: 8px 24px;
+box-sizing: border-box;
+line-height: 28px;
+margin-top: 100px;
+
+&::after {
+ clear: both;
+ content: '';
+ display: block; 
+}
+
+& h1 {
+  float: left;
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 48px;
+}
+
+& .all_show {
+  float: right;
+  font-size: 18px;
+  font-weight: 700;
+  color: #5C5C5C;
+
+  & span:first-of-type {
+    margin: 10px 0;
+    display: inline-block;
+    margin-right: 16px;
+  }
+  & .triangle {
+     display: inline-block;
+     width: 0;
+     height: 0;
+     border-top: 7px solid transparent;
+     border-left: 9px solid #5C5C5C;
+     border-bottom: 7px solid transparent;
+  }
+`;
+
+export const BreadShopList = styled.div`
+  outline: 1px solid red;
+
+  .list_wrap {
+    padding-left: 24px;
+    padding-right: 24px;
+    margin-bottom: 26px;
+    width: 100%;
+    box-sizing: border-box;
+    
+   
+    &::after {
+      clear: both;
+      content: '';
+      display: block; 
+    }
+  
+  
+    & li {
+      float: left;
+      margin-right: 40px;
+      // outline: 1px solid red;
+      margin-bottom: 26px;
+  
+     :nth-child(4) {
+       margin-right: 0;
+     }
+     :nth-child(8) {
+       margin-right: 0;
+     }
+     
+      & img{
+        width: 214px;
+        height: 214px;
+        display: block;
+      } 
+    }
+  
+    & dl {
+      margin-top: 24px;
+  
+      & dt {
+        margin-bottom: 8px;
+        background-color: #0CC3A3;
+        border-radius: 4px;
+        width: 80px;
+        padding: 0 8px;
+        color: white;
+        height: 24px;
+        line-height: 24px;
+        font-size: 14px;
+      text-align: center;
+      }
+      & dd {
+        margin-bottom: 24px;
+        font-size: 24px;
+        font-weight: 700;
+        
+      }
+    }
 `;
